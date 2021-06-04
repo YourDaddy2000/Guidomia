b27ca59a7be21d71a16fab536902703164c47706
Roman B <roma.bozhenko@gmail.com>
2021-06-04 10:05:58 +0300

    Initial Commit

---
+++ Guidomia_MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guidomia_MVP
+//
+//  Created by Roman Bozhenko on 04.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
