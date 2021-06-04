8a3e6e740596d776f3909001e93d501c7b5451c9
Roman B <roma.bozhenko@gmail.com>
2021-06-04 17:32:35 +0300

    set nav bar ui

---
+++ Guidomia_MVP/Common/Extensions/Extension+UIColor.swift
@@ -0,0 +1,12 @@
+//
+//  Extension+UIColor.swift
+//  Guidomia_MVP
+//
+//  Created by Roman Bozhenko on 04.06.2021.
+//
+
+import UIKit
+
+extension UIColor {
+    static let navBarOrange = #colorLiteral(red: 0.9883627295, green: 0.3786247373, blue: 0.08803100139, alpha: 1)
+}
--- Guidomia_MVP/Supporting Files/SceneDelegate.swift
+++ Guidomia_MVP/Supporting Files/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
-        let navigationController = UINavigationController()
+        let navigationController = BaseNavigationController()
         
         window = UIWindow(windowScene: scene)
         window!.rootViewController = navigationController
