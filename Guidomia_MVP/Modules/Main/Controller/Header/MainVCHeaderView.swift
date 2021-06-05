d724c0c15a6738086fdac10d64cbc6b86a4ab7c6
Roman B <roma.bozhenko@gmail.com>
2021-06-05 12:27:25 +0300

    add table view header

---
+++ Guidomia_MVP/Modules/Main/Controller/Header/MainVCHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  MainVCHeaderView.swift
+//  Guidomia_MVP
+//
+//  Created by Roman Bozhenko on 04.06.2021.
+//
+
+import UIKit
+
+class MainVCHeaderView: UITableViewHeaderFooterView {
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+}
