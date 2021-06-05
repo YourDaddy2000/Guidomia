9db659a84828d37aefa48d2f2045baa547295bde
Roman B <roma.bozhenko@gmail.com>
2021-06-05 14:47:39 +0300

    add table view cell, add assets

---
+++ Guidomia_MVP/Modules/Main/Controller/Cells/MainVCExpandableCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainVCExpandableCell.swift
+//  Guidomia_MVP
+//
+//  Created by Roman Bozhenko on 04.06.2021.
+//
+
+import UIKit
+
+class MainVCExpandableCell: UITableViewCell {
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var bottomSpacerView: UIView!
+    
+    
+}
